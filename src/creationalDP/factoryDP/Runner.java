998203130c8e79157421ad06797746a0be64434f
halil <halilakgun0@gmail.com>
2023-05-06 10:43:29 +0300

    Initial commit

---
+++ src/creationalDP/factoryDP/Runner.java
@@ -0,0 +1,16 @@
+package creationalDP.factoryDP;
+
+public class Runner {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.draw();
+
+        Square square = new Square();
+        square.draw();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
+
+
+    }
+}
